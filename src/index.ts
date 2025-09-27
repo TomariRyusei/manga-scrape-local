23141cb7d99932b5f93b2e6953cdd587951b12b5
TomariRyusei <t.ryu6635@gmail.com>
2025-09-28 00:16:05 +0900

    環境変数の読み込みを整備

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const LINE_NOTIFY_TOKEN = process.env.LINE_CHANNEL_ACCSESS_TOKEN;
+const LINE_USER_ID = process.env.LINE_USER_ID;
+
+const EMAIL_CONFIG = {
+  auth: {
+    user: process.env.MAIL_ADDRESS,
+    pass: process.env.MAIL_SERVICE_PASSWORD,
+  },
+};
+const RECIPIENT_EMAIL = process.env.MAIL_ADDRESS;
--- start-app.sh
+++ start-app.sh
@@ -4,6 +4,6 @@ set -e
 echo "=== $(date '+%Y-%m-%d %H:%M:%S') - Starting Node App ==="
 
 cd /Users/tryu/dev/manga-scrape-local || exit 1
-/opt/homebrew/bin/node ./dist/index.js
+/opt/homebrew/bin/node --env-file=.env ./dist/index.js
 
 echo "=== $(date '+%Y-%m-%d %H:%M:%S') - Completed Successfully ==="
